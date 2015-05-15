3c6639c2f0d2944b39cfeba16e0fc4179f0590b6
lsCoolJ <bfoster07703@gmail.com>
2015-05-15 10:21:00 -0700

    removed unnecessary files

--- Robomagellan/Navigation/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-g++ driver.cpp -I . -I ./boost_1_55_0/ -o output -std=c++11 $@
-
-exit 0
-
--- Robomagellan/arduino/robomagellan/command.ino
+++
@@ -1,9 +0,0 @@
-/**
- * @brief       Command interface
- * 
- * @file        command.ino
- * @author      Chad Condon
- * @date        2014-05-28
- * 
- * @details     Listens for and interprets serial commands.
- */
